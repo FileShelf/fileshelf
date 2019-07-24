238b4fb35f8248667a739f82748bddab251315c8
Tom Lutzenberger <thomas.lutzenberger@ostendis.com>
2019-07-24 14:02:03 +0200

    [:heavy_plus_sign: ADD DEPENDENCY] Install Deployer
    Fixes ---
    Related to ---

---
+++ migrations/m190722_221209_initialization.php
@@ -0,0 +1,57 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190722_221209_initialization
+ */
+class m190722_221209_initialization extends Migration
+{
+    protected $yiiMigrations = [
+        //'m141106_185632_log_init'                                  => 'vendor/yiisoft/yii2/log/migrations/',
+        'm150207_210500_i18n_init'                                 => 'vendor/yiisoft/yii2/i18n/migrations/',
+        //'m150909_153426_cache_init'                                => 'vendor/yiisoft/yii2/caching/migrations/',
+        'm140506_102106_rbac_init'                                 => 'vendor/yiisoft/yii2/rbac/migrations/',
+        'm170907_052038_rbac_add_index_on_auth_assignment_user_id' => 'vendor/yiisoft/yii2/rbac/migrations/',
+        'm180523_151638_rbac_updates_indexes_without_prefix'       => 'vendor/yiisoft/yii2/rbac/migrations/',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        foreach ($this->yiiMigrations as $class => $path) {
+            require_once $path . $class . '.php';
+            (new $class())->up();
+        }
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        foreach (array_reverse($this->yiiMigrations) as $class => $path) {
+            require_once $path . $class . '.php';
+            (new $class())->down();
+        }
+
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190722_221209_initialization cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
