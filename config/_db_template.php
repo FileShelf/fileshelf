f0afb9d1508f4dadb8d8158ed96907a761f8b31d
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2020-01-30 23:12:57 +0100

    [:wrench: CONFIG] Add db template and adjust gitignore

---
+++ config/_db_template.php
@@ -0,0 +1,22 @@
+<?php
+
+if(YII_ENV_PROD) {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=127.0.0.1;dbname=my_database',
+        'username' => 'root',
+        'password' => 'password',
+        'charset' => 'utf8',
+
+        'enableQueryCache' => !YII_DEBUG,
+        'queryCacheDuration' => 3600,
+    ];
+}
+
+return [
+    'class'    => 'yii\db\Connection',
+    'dsn'      => 'mysql:host=127.0.0.1;dbname=my_database',
+    'username' => 'root',
+    'password' => 'password',
+    'charset'  => 'utf8',
+];
