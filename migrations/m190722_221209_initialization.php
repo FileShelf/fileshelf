529a647c40cdf8bb0cec5bac34acd19b483bea24
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2020-12-21 22:31:38 +0100

    #000: Upgrade migration logic

--- migrations/m190722_221209_initialization.php
+++ migrations/m190722_221209_initialization.php
@@ -7,6 +7,7 @@ use yii\db\Migration;
  */
 class m190722_221209_initialization extends Migration
 {
+
     protected $yiiMigrations = [
         //'m141106_185632_log_init'                                  => 'vendor/yiisoft/yii2/log/migrations/',
         'm150207_210500_i18n_init'                                 => 'vendor/yiisoft/yii2/i18n/migrations/',
@@ -16,42 +17,31 @@ class m190722_221209_initialization extends Migration
         'm180523_151638_rbac_updates_indexes_without_prefix'       => 'vendor/yiisoft/yii2/rbac/migrations/',
     ];
 
+
     /**
      * {@inheritdoc}
      */
-    public function safeUp()
+    public function up() : bool
     {
         foreach ($this->yiiMigrations as $class => $path) {
             require_once $path . $class . '.php';
             (new $class())->up();
         }
 
+        return true;
     }
+
 
     /**
      * {@inheritdoc}
      */
-    public function safeDown()
+    public function down() : bool
     {
         foreach (array_reverse($this->yiiMigrations) as $class => $path) {
             require_once $path . $class . '.php';
             (new $class())->down();
         }
 
+        return true;
     }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m190722_221209_initialization cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
